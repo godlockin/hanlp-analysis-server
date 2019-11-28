06872ec1e7e84a2ef79a4195b7268e7c20b38820
godlockin <chen.chen@aigauss.com>
2019-11-28 18:02:15 +0800

    fix method

--- src/main/java/com/controller/AnalysisController.java
+++ src/main/java/com/controller/AnalysisController.java
@@ -4,6 +4,7 @@ import com.model.BaseRequest;
 import com.service.langid.DetectedLanguage;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,12 +12,12 @@ import java.util.List;
 @RestController
 public class AnalysisController extends BaseController {
 
-    @RequestMapping(value = "/langId")
+    @RequestMapping(value = "/langId", method = RequestMethod.POST)
     public DetectedLanguage doLangId(@RequestBody BaseRequest request) {
         return landIdService.doAnalysis(request);
     }
 
-    @RequestMapping(value = "/analyze")
+    @RequestMapping(value = "/analyze", method = RequestMethod.POST)
     public List<String> doAnalysis(@RequestBody BaseRequest request) {
         return hanlpService.doAnalysis(request);
     }
