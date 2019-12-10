8a555e9ec8b376082e375118efbe0d5f228bf763
godlockin <chen.chen@aigauss.com>
2019-12-10 15:26:04 +0800

    fix health check

--- src/main/java/com/controller/HealthController.java
+++ src/main/java/com/controller/HealthController.java
@@ -1,12 +1,12 @@
 package com.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HealthController {
 
-    @RequestMapping(value = "/_health")
+    @RequestMapping(value = "")
     public int healthCheck() {
         return 200;
     }
